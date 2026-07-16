39598a7a4e4ae64f608ea8250142260f3d21551a
Xingyu Chen <Asixa@foxmail.com>
2026-07-15 22:50:49 -0700

    test: freeze solver pipeline boundaries

--- tests/test_import_graph_contract.py
+++ tests/test_import_graph_contract.py
@@ -29,7 +29,7 @@ def test_current_import_debt_is_exact_and_allowlisted():
     assert Counter(
         graph._DEBT_GROUP_BY_RULE[violation.rule] for violation in violations
     ) == {
-        "existing_boundary": 6,
+        "existing_boundary": 2,
     }
 
 
